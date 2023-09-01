1a7fcd8811d247339b9b538b7f7092178bdc8164
AnriWulaia <wulaia.anri@gmail.com>
2023-09-01 10:56:47 +0400

    added cookies

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,15 @@
 ﻿function returnToDefaultPage() {
     window.location.href = '/'; 
 }
+
+var successMessage = Cookies.get('success');
+
+if (successMessage) {
+    Cookies.remove("success");
+    console.log("su");
+    var successMessageDiv = document.getElementById("success");
+    successMessageDiv.style.display = "flex";
+    setTimeout(function () {
+        successMessageDiv.style.display = "none";
+    }, 3000);
+}
