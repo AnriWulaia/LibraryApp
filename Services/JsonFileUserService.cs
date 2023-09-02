beb73ec38ceea57255d542df23d26b72fe6f3921
AnriWulaia <wulaia.anri@gmail.com>
2023-09-02 21:53:28 +0400

    added user page

---
+++ Services/JsonFileUserService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.Text.Json;
+using Library.models;
+using Newtonsoft.Json;
+
+namespace Library.Services
+{
+    public class JsonFileUserService
+    {
+
+        public JsonFileUserService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "users.json");
+        
+        public List<Users> GetUsers()
+        {
+            using StreamReader reader = new(JsonFileName);
+            var json = reader.ReadToEnd();
+            List<Users> users = JsonConvert.DeserializeObject<List<Users>>(json);
+            return users;
+        }
+    }
+
+
+}
---
+++ models/Users.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.models
+{
+    public class Users
+    {
+        [BindProperty]
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = "";
+
+        [BindProperty]
+        [Required(ErrorMessage = "Surname is required.")]
+        public string Surname { get; set; } = "";
+
+        [BindProperty]
+        [Required(ErrorMessage = "Number is required.")]
+        public string Number { get; set; } = "";
+
+        [BindProperty]
+        public List<string>? Borrowed { get; set; }
+    }
+}
